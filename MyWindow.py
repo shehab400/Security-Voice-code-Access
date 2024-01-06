1f35b833636b0017ef6e8879f5ac3fc9bab84140
AbdelRahman Hesham <dollarouty@hotmail.com>
2024-01-06 20:03:37 +0200

    revert random training

--- MyWindow.py
+++ MyWindow.py
@@ -199,10 +199,10 @@ class MyWindow(QMainWindow):
             yt_random = y_random[:24]
             ys_random = y_random[24:]
 
-            X_train=np.concatenate((Xt_open, Xt_grant, Xt_unlock, Xt_random), axis=0)
-            X_test=np.concatenate((Xs_open, Xs_grant, Xs_unlock, Xs_random), axis=0)
-            y_train=np.concatenate((yt_open, yt_grant, yt_unlock, yt_random), axis=0)
-            y_test=np.concatenate((ys_open, ys_grant, ys_unlock, ys_random), axis=0)
+            X_train=np.concatenate((Xt_open, Xt_grant, Xt_unlock), axis=0)
+            X_test=np.concatenate((Xs_open, Xs_grant, Xs_unlock), axis=0)
+            y_train=np.concatenate((yt_open, yt_grant, yt_unlock), axis=0)
+            y_test=np.concatenate((ys_open, ys_grant, ys_unlock), axis=0)
 
 
 
@@ -225,11 +225,11 @@ class MyWindow(QMainWindow):
             elif y_pred[0] == 2:
                 self.ui.progressBar_15.setValue(similarity.randint(90,100))
                 self.textLabel.setText("ACCESS GRANTED😁")
-            elif y_pred[0] == 3:
-                self.ui.progressBar_13.setValue(similarity.randint(0,60))
-                self.ui.progressBar_14.setValue(similarity.randint(0,60))
-                self.ui.progressBar_15.setValue(similarity.randint(0,60))
-                self.textLabel.setText("ACCESS DENIED😢")
+            # elif y_pred[0] == 3:
+            #     self.ui.progressBar_13.setValue(similarity.randint(0,60))
+            #     self.ui.progressBar_14.setValue(similarity.randint(0,60))
+            #     self.ui.progressBar_15.setValue(similarity.randint(0,60))
+            #     self.textLabel.setText("ACCESS DENIED😢")
             return y_pred[0]
 
     def recognizeSpeaker(self):
